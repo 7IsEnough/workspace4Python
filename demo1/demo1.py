d309f92ef2effcd7364cfaefbe59f107666729fa
Promise <976949689@qq.com>
2021-06-26 00:00:14 +0800

    2021年6月26日00:00:07

---
+++ demo1/demo1.py
@@ -0,0 +1,13 @@
+# -*- coding = utf-8 -*-
+# @Author: 97694
+# @Time: 2021/6/25 22:58
+
+# 这是我的第一个python程序
+print("hello,world")   # 这是单行注释
+
+'''
+这是第一行注释
+这是第二行注释
+'''
+
+print("python");
---
+++ demo1/demo2.py
@@ -0,0 +1,23 @@
+# -*- coding = utf-8 -*-
+# @Author: 97694
+# @Time: 2021/6/25 23:08
+
+# print("标准化输出字符串")
+#
+# a = 10
+#
+# print("这是变量：", a)
+
+# 格式化输出
+age = 18
+
+print("我的名字是%s，我的国籍是%s" % ("小张", "中国"))
+
+print("我的年纪是：%d岁" % age)
+
+print("aaa", "bbb", "ccc")
+print("www", "baidu", "com", sep=".")
+print("hello", end="")
+print("world", end="\t")
+print("python", end="\n")
+print("end")
